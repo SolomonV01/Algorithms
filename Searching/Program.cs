d38961e800a41883a8a90e1277aebda950de44d9
SolomonV01 <113135388+SolomonV01@users.noreply.github.com>
2023-12-20 22:04:59 -0600

    Search

---
+++ Searching/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+
+namespace Searching;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BinarySearch binarySearch = new BinarySearch();
+        binarySearch.Start();
+
+        ReadLine();
+        Clear();
+
+        InterpolationSearch interpolationSearch = new InterpolationSearch();
+        interpolationSearch.Start();
+
+        ReadLine();
+        Clear();
+
+        LinearSearch linearSearch = new LinearSearch();
+        linearSearch.Start();
+
+        ReadLine();
+    }
+}
+
